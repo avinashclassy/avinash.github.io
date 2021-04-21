98cadf30aafff21373e6f3ac8ea7201a78321951
avinashclassy <50662335+avinashclassy@users.noreply.github.com>
2021-04-21 19:41:24 +0530

    Add files via upload

---
+++ build/precache-manifest.4366ec3a87d7dbdd50252574db199ca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "629dbbaa630f3e419ee6",
+    "url": "/static/css/main.d363aca0.chunk.css"
+  },
+  {
+    "revision": "629dbbaa630f3e419ee6",
+    "url": "/static/js/main.fc040f1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d70e9571cbe106007dc6",
+    "url": "/static/js/2.5ebde9bc.chunk.js"
+  },
+  {
+    "revision": "8b0c62c9e84f775bda01bce3ed19b010",
+    "url": "/index.html"
+  }
+];
